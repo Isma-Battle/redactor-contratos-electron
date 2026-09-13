5355d8af4be8ac40c056d9e09c578d9692824d98
Isma-Battle <ismaga525@gmail.com>
2026-09-12 23:53:34 -0400

    ICONO

--- main.js
+++ main.js
@@ -62,6 +62,7 @@ function createWindow() {
     minHeight: 640,
     backgroundColor: '#1E2A38',
     title: 'Redactor de Contratos',
+    icon: path.join(__dirname, 'build', 'icon.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
